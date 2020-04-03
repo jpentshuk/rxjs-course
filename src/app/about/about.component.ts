34fda551e54e052a02008cff23401c8ee1153442
janpentsuk <jan@ivotingcentre.ee>
2020-04-03 10:54:07 +0300

    lesson_7_observable-introduction

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -26,22 +26,20 @@ import {createHttpObservable} from '../common/util';
 export class AboutComponent implements OnInit {
 
     ngOnInit() {
-      document.addEventListener('click', evt => {
-        console.log(evt);
-        setTimeout(()=> {
-          console.log('finished');
-          let counter = 0;
-          setInterval(() => {
-            console.log(counter);
-            counter++;
-          }, 1000);
-        }, 3000);
-      });
-      // so yes
-      // streams are basically all those: setTimeouts, setIntervals, HTTP requests, because those all are asyncronous
+      const interval$ = interval(1000); // its definition of stream of values as known as Observable
 
-      // so its nested - so called callback hell, and there Rxjs comes handy
-      // Rxjs - reactive extensions for javascript
+      // we need to subscribe to make stream of values
+
+      interval$.subscribe(val => console.log('stream 1 =>  ' + val));
+      interval$.subscribe(val => console.log('stream 2 =>  ' + val));
+
+      // wait 3 seconds and then start to emit values each second
+      const intervalAfterSomeTime$ = timer(3000, 1000);
+      intervalAfterSomeTime$.subscribe(val => console.log('stream 1 after 3 secs ' + val));
+
+      // click streams
+      const click$ = fromEvent(document, 'click');
+      click$.subscribe(evt => console.log(evt));
     }
 }
 
