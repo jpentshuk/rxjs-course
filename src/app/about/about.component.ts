4e10783e3933cd3d5c66d8e4b2bc5a52313cc589
janpentsuk <jan@ivotingcentre.ee>
2020-04-03 10:22:23 +0300

    lesson_5_streams

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -26,6 +26,19 @@ import {createHttpObservable} from '../common/util';
 export class AboutComponent implements OnInit {
 
     ngOnInit() {
+      document.addEventListener('click', evt => {
+        console.log(evt);
+      });
+
+      let counter = 0;
+      setInterval(() => {
+        console.log(counter);
+        counter++;
+      }, 1000);
+
+      setTimeout(()=> {
+        console.log('finished');
+      }, 3000);
 
 
     }
